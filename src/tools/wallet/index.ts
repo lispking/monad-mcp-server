850cc6b0e2626540d7dcf4435157609f2065870b
lispking <lispking@qq.com>
2025-04-26 09:34:05 +0800

    make code tidy

--- src/tools/wallet/index.ts
+++ src/tools/wallet/index.ts
@@ -1,6 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { balanceProvider } from "./balance";
-import { sendMonProvider } from "./transaction";
+import { sendMonProvider } from "./transfer";
 
 export function walletProvider(server: McpServer) {
     balanceProvider(server);
